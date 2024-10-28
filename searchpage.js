b402ba9d7d3db03375ab2f21879503fb98e9f33c
Lakshmiyasaswini <yashuraja1708@gmail.com>
2024-10-28 09:56:53 +0530

    Add files via upload

--- searchpage.js
+++ searchpage.js
@@ -1,23 +1,28 @@
-import RegisterPage from "../../support/pageclasses/Register";
-import SearchPage from "../../support/pageclasses/searchpage";
+class SearchPage{
+    
+    Enterurl(){
+        cy.visit('https://tutorialsninja.com/demo/');
 
-describe('search page', () => {
-
-    const searchobj = new SearchPage
-    const registerobj = new RegisterPage
-
-    it('Add to cart', () => {
-        // Enter url
-        registerobj.Enterurl();
-
-        // Register
-        registerobj.RegisterPage();
-
-        // Enter url
-        searchobj.Enterurl();
-
-        // Search add to cart
-        searchobj.SearchPage();
+    }
+   
+    SearchPage(){
+        // Enter the product name in the search box
+        cy.get('[name="search"]').type('HP LP3065');
         
-    });
-});
+        // Click on the search button
+        cy.get('.btn.btn-default.btn-lg').click();
+        
+        // click on the add to cart
+        cy.get(`[onclick="cart.add('47', '1');"]`).click()
+        
+        // add quantity
+        cy.get('#input-quantity').clear().type('2')
+        cy.get('[id="button-cart"]').click();
+        
+        // Assertion to validate successful addition to cart
+        cy.get('[class="alert alert-success alert-dismissible"]').should('contain', 'Success: You have added');        
+
+    }
+}
+export default SearchPage;
+    
